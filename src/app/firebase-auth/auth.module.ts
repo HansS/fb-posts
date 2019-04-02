b583bc0332f993ba6268e15505d61723ac0ab94f
HansS <gian.schenker@gmail.com>
2019-04-02 16:44:05 +0200

    new stuff

--- src/app/firebase-auth/auth.module.ts
+++ src/app/firebase-auth/auth.module.ts
@@ -8,9 +8,12 @@ import { AuthComponent } from "./auth.component";
 import { LoginComponent } from "./login/login.component";
 import { SigninComponent } from "./signin/signin.component";
 import { FirebaseAngularModule } from "../firebase-angular/firebase-angular.module";
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
-  declarations: [AuthComponent, LoginComponent, SigninComponent],
+  declarations: [AuthComponent, LoginComponent, SigninComponent, UserComponent, UserListComponent, UserEditComponent],
   imports: [
     CommonModule,
     FirebaseAngularModule,
